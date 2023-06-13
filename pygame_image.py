5b4742aac3f2a5f3c2264e87e9a88187fa61e90d
manato <c0b22062c9@edu.teu.ac.jp>
2023-06-13 17:30:46 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)] * 2
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 8, 1.0), pg.transform.rotozoom(kk_img, 12, 1.0)]
     tmr = 0
     x = 0
 
@@ -20,11 +20,11 @@ def main():
         x = tmr % 3200 
         for i in  range(4):
             screen.blit(bg_imgs[i], [1600 * i - x, 0])
-        screen.blit(kk_img[tmr % 2], [300, 200])
+        screen.blit(kk_imgs[tmr % 100 // 25], [300, 200])
 
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
